ba7bffe44f2b1ca200c77b6f464d86aab4169727
sergi-ss <sergi@thepalace.dev>
2023-11-21 20:53:34 +0200

    [responsive] logistics page 1.0

--- src/components/Card8/index.jsx
+++ src/components/Card8/index.jsx
@@ -1,21 +1,11 @@
-import React from 'react'
-import { Box, Typography, } from '@mui/material'
+import React from 'react';
+import { Box, Typography } from '@mui/material';
 
-// import SVG from '../src/assets/svg/SVG.svg'
-// const content={
-//   shadowColor1: "10px 10px 0px -2px rgba(255, 179, 34, 0.52)",
-//   shadowColor2: "10px 10px 0px -2px rgba(90, 165, 255, 0.53)",
-//   image: SVG,
-//   header: 'Warehousing Facilities',
-//   text: 'Each production unit is equipped with self-contained open and losed warehouses designed for the storage of raw materials, onsumables,        packing materials, and a dedicated space for finished goods. The total        covered storage area spans 100,000 square feet, including a 50,000        square feet warehouse located at Mundra port. Additionally, there is an        open storage facility covering approximately 400,000 square feet. All        manufacturing units are outfitted with a range of customized packaging        solutions to meet specific requirements.'
-// }
-
-function Card8 ({content}) {
-
+function Card8({ content }) {
   return (
     <Box
-      sx={{
-        height: "466px",
+      style={{
+        height: "auto",
         justifyContent: 'center',
         alignItems: 'start',
         borderRadius: '20px',
@@ -23,7 +13,10 @@ function Card8 ({content}) {
         backgroundColor: '#f1f4f6',
         display: 'flex',
         flexDirection: 'column',
-        padding: '50px'
+        padding: '20px',
+        '@media (min-width: 600px)': {
+          flexDirection: 'row',
+        },
       }}
     >
       <img
@@ -31,37 +24,42 @@ function Card8 ({content}) {
         src={content.image}
         style={{
           width: '95px',
-          marginBottom: "7px"
+          height: '95px',
+          marginRight: '10px',
         }}
         alt='Warehousing Facilities'
       />
-      <Typography
-        sx={{
+      <Box
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
           justifyContent: 'center',
-          alignSelf: 'stretch',
-          fontFamily: 'Manrope, -apple-system, Roboto, Helvetica, sans-serif',
-          fontWeight: '700',
-          fontSize: '32px',
-          lineHeight: '32px',
-          padding: "20px 0 20px 0"
         }}
       >
-        {content.header}
-      </Typography>
-      <Typography
-        sx={{
-          justifyContent: 'center',
-          width: '674px',
-          fontFamily: 'Manrope, -apple-system, Roboto, Helvetica, sans-serif',
-          fontWeight: '600',
-          fontSize: '16px',
-          lineHeight: '24px'
-        }}
-      >
-        {content.text}
-      </Typography>
+        <Typography
+          style={{
+            fontFamily: 'Manrope, -apple-system, Roboto, Helvetica, sans-serif',
+            fontWeight: '700',
+            fontSize: '24px',
+            lineHeight: '32px',
+            marginBottom: '10px',
+          }}
+        >
+          {content.header}
+        </Typography>
+        <Typography
+          style={{
+            fontFamily: 'Manrope, -apple-system, Roboto, Helvetica, sans-serif',
+            fontWeight: '600',
+            fontSize: '16px',
+            lineHeight: '24px',
+          }}
+        >
+          {content.text}
+        </Typography>
+      </Box>
     </Box>
-  )
+  );
 }
 
-export default Card8
+export default Card8;
--- src/pages/Logictics/sections/Main1/index.jsx
+++ src/pages/Logictics/sections/Main1/index.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 import { Grid, Typography } from '@mui/material'
 import MainLogo from '../../../../assets/Logistics/SVG/main.svg'
 import './style.css'
-function Main2 () {
+
+function Main2() {
   return (
     <Grid
       container
       direction='column'
       alignItems='center'
-      sx={{ marginTop: '96px' }}
+      sx={{
+        marginTop: '96px',
+        padding: { xs: '10px', sm: '50px 20px', md: '106.8px 153.59px 10px 153.59px' }
+      }}
+
     >
       <Grid
         container
@@ -17,33 +22,33 @@ function Main2 () {
         alignItems='center'
         justifyContent='center'
         sx={{
-          width: '1574.41px',
-          height: '168px',
+          width: '100%',
+          maxWidth: '1574.41px',
           borderRadius: '20px',
-          boxShadow: '10px 10px 0px 0px rgba(0, 26, 57, 0.93)'
+          boxShadow: '10px 10px 0px 0px rgba(0, 26, 57, 0.93)',
+          padding: '20px',
         }}
       >
-        <Grid item md={12} sx={{ display: 'flex' }}>
+        <Grid item md={12} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
           <img
             alt=''
             src={MainLogo}
             style={{
               width: '82px',
               height: '82px',
-              marginLeft: '40px'
+              marginBottom: '20px',
             }}
           />
-          <div
-            style={{
+          <Typography
+            variant='body1'
+            sx={{
               color: '#FFF',
-              fontFamily:
-                'Manrope, -apple-system, Roboto, Helvetica, sans-serif',
+              fontFamily: 'Manrope, -apple-system, Roboto, Helvetica, sans-serif',
               fontSize: '16px',
               fontStyle: 'normal',
               fontWeight: 500,
               lineHeight: '24px',
-              margin: "auto",
-              paddingLeft: "15px"
+              textAlign: 'center',
             }}
           >
             Liks boasts an in-house shipping and logistics team proficient in
@@ -55,7 +60,7 @@ function Main2 () {
             palletization quality and the optimal utilization of container
             space. This meticulous approach guarantees the safe stuffing of the
             maximum possible cargo under secure conditions.
-          </div>
+          </Typography>
         </Grid>
       </Grid>
     </Grid>
